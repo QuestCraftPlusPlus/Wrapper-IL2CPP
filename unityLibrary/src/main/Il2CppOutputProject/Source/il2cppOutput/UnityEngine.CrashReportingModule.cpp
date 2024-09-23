6489c715d2c69643a5e05b3eca5f48f9eb012703
The Judge <colepatirckroberts@gmail.com>
2024-09-22 22:45:52 -0700

    Login fixes

---
+++ unityLibrary/src/main/Il2CppOutputProject/Source/il2cppOutput/UnityEngine.CrashReportingModule.cpp
@@ -0,0 +1,122 @@
+﻿#include "pch-cpp.hpp"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include <limits>
+
+
+
+// System.String
+struct String_t;
+
+
+
+IL2CPP_EXTERN_C_BEGIN
+IL2CPP_EXTERN_C_END
+
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+
+// <Module>
+struct U3CModuleU3E_t62B8AA732FD56BBF041B9F0091F091311788A0F6 
+{
+};
+
+// UnityEngine.CrashReportHandler.CrashReportHandler
+struct CrashReportHandler_tDC3582120286C333EE85347A7CE6C793B1B2B019  : public RuntimeObject
+{
+};
+
+// System.ValueType
+struct ValueType_t6D9B272BD21782F0A9A14F2E41F85A50E97A986F  : public RuntimeObject
+{
+};
+// Native definition for P/Invoke marshalling of System.ValueType
+struct ValueType_t6D9B272BD21782F0A9A14F2E41F85A50E97A986F_marshaled_pinvoke
+{
+};
+// Native definition for COM marshalling of System.ValueType
+struct ValueType_t6D9B272BD21782F0A9A14F2E41F85A50E97A986F_marshaled_com
+{
+};
+
+// System.Boolean
+struct Boolean_t09A6377A54BE2F9E6985A8149F19234FD7DDFE22 
+{
+	// System.Boolean System.Boolean::m_value
+	bool ___m_value_0;
+};
+
+// System.Void
+struct Void_t4861ACF8F4594C3437BB48B6E56783494B843915 
+{
+	union
+	{
+		struct
+		{
+		};
+		uint8_t Void_t4861ACF8F4594C3437BB48B6E56783494B843915__padding[1];
+	};
+};
+
+// <Module>
+
+// <Module>
+
+// UnityEngine.CrashReportHandler.CrashReportHandler
+
+// UnityEngine.CrashReportHandler.CrashReportHandler
+
+// System.Boolean
+struct Boolean_t09A6377A54BE2F9E6985A8149F19234FD7DDFE22_StaticFields
+{
+	// System.String System.Boolean::TrueString
+	String_t* ___TrueString_5;
+	// System.String System.Boolean::FalseString
+	String_t* ___FalseString_6;
+};
+
+// System.Boolean
+
+// System.Void
+
+// System.Void
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+
+
+
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+// System.Void UnityEngine.CrashReportHandler.CrashReportHandler::set_enableCaptureExceptions(System.Boolean)
+IL2CPP_EXTERN_C IL2CPP_METHOD_ATTR void CrashReportHandler_set_enableCaptureExceptions_m43B6DDC35A11833111069353501CDA7EE2D75351 (bool ___0_value, const RuntimeMethod* method) 
+{
+	typedef void (*CrashReportHandler_set_enableCaptureExceptions_m43B6DDC35A11833111069353501CDA7EE2D75351_ftn) (bool);
+	static CrashReportHandler_set_enableCaptureExceptions_m43B6DDC35A11833111069353501CDA7EE2D75351_ftn _il2cpp_icall_func;
+	if (!_il2cpp_icall_func)
+	_il2cpp_icall_func = (CrashReportHandler_set_enableCaptureExceptions_m43B6DDC35A11833111069353501CDA7EE2D75351_ftn)il2cpp_codegen_resolve_icall ("UnityEngine.CrashReportHandler.CrashReportHandler::set_enableCaptureExceptions(System.Boolean)");
+	_il2cpp_icall_func(___0_value);
+}
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
---
+++ unityLibrary/src/main/Il2CppOutputProject/Source/il2cppOutput/UnityEngine.CrashReportingModule_CodeGen.c
@@ -0,0 +1,45 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void UnityEngine.CrashReportHandler.CrashReportHandler::set_enableCaptureExceptions(System.Boolean)
+extern void CrashReportHandler_set_enableCaptureExceptions_m43B6DDC35A11833111069353501CDA7EE2D75351 (void);
+static Il2CppMethodPointer s_methodPointers[1] = 
+{
+	CrashReportHandler_set_enableCaptureExceptions_m43B6DDC35A11833111069353501CDA7EE2D75351,
+};
+static const int32_t s_InvokerIndices[1] = 
+{
+	10125,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_UnityEngine_CrashReportingModule_CodeGenModule;
+const Il2CppCodeGenModule g_UnityEngine_CrashReportingModule_CodeGenModule = 
+{
+	"UnityEngine.CrashReportingModule.dll",
+	1,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
